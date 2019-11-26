683fa5f83d6331c146b61756b3b4cc31360e739e
Enrique Kreuk <Enrique.Kreuk@zzm.local>
2019-11-26 10:50:58 +0100

    Removed unused code.

--- index.php
+++ index.php
@@ -5,34 +5,6 @@ error_reporting(E_ALL);
 
 include_once('src/Class.App.php');
 
-// $router = new Router();
-
-// $router->get("/users/all/test/{werkt}", function($param){
-//     echo "users func " . $param["werkt"];
-// });
-
-// $router->get("/user/{id}", function($param){
-//     Response::Json(["userID" => $param['id']]);
-//     echo $_SERVER['REQUEST_METHOD'];
-// });
-
-// $router->get("/user/{id}/frikando/naam/{naam}/info/lang/{taal}", function($param){
-//     Response::Json(
-//         [
-//             "userID" => $param['id'], 
-//             "taal" => $param["taal"]
-//         ]
-//     );
-//     echo $_SERVER['REQUEST_METHOD'];
-// });
-
-// $router->put("/user/{id}", function($param){
-//     Response::Json(["userID" => $param['id']]);
-//     echo "ik ben een put request";
-// });
-
-//$router->Run();
-
 $app = new App();
 
 $app->Router->get("/user/{id}", function($param){
