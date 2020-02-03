9b0bb54678a24c990ffbee6f3ed5aa548a8d4a12
Enrique <enrique.kreuk@icloud.com>
2020-02-03 21:58:53 +0100

    Added post and get methods

--- admin/frontend/src/Components/Collections.js
+++ admin/frontend/src/Components/Collections.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { hasSession } from './Helpers';
 import Nav from './Nav';
-import check from '../checked.png';
 
 class Collections extends React.Component {
     constructor(props){
@@ -9,12 +8,50 @@ class Collections extends React.Component {
         super(props);
 
         this.state = {
-          collections: null,
+          SelectedCollection : null,
+          Collections: [],
         };
+
+        this.New = this.New.bind(this);
     }
 
     componentDidMount(){
-        console.log(check);
+        this.Load();
+    }
+
+    Save(name, description){
+      fetch('http://localhost/api/collection/create', {
+          method : "POST",
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(
+            { 
+              collection : {
+                name: name,
+                description: description,
+                owner : 14
+              }
+            }
+            )
+      })
+      .then(function(data){
+          console.log(data);
+          if(data.status === 201){
+              alert("Collection has been created");
+          }
+          else{
+              alert("Error while updating page");
+          }
+      })
+      .catch(function(error){
+          this.setState(
+              {
+                  type : "error-messages",
+                  messages : "Something went wrong..."
+              });
+      }.bind(this));
     }
 
     New(){
@@ -22,15 +59,61 @@ class Collections extends React.Component {
         let li = document.createElement('li');
         li.append(inp)
         let coll = document.getElementById("collection-list");
-        let check = document.createElement('img');
-        check.src = {check}
-        li.append(check)
+
+        let acceptButton = document.createElement('button');
+        acceptButton.innerHTML = "Save";
+
+        acceptButton.addEventListener('click', function(){
+          const name = inp.value;
+          li.removeChild(inp);
+          const col = document.createElement("strong");
+          col.innerHTML = name;
+
+          li.innerHTML = "";
+          
+          li.append(col);
+
+          let colls = this.state.Collections;
+
+          colls.push({
+            Name : name,
+            Types : []
+          });
+
+          this.Save(name, "");
+
+          this.setState({
+              SelectedCollection : 0,
+              Collections : colls
+          });
+
+        }.bind(this));
+
+        let cancelButton = document.createElement('button');
+        cancelButton.innerHTML = "Cancel";
+
+        li.append(acceptButton)
+        li.append(cancelButton)
         coll.append(li)
-        console.log('a');
+    }
+
+    Load(){
+      fetch("http://localhost/api/collection/list/all")
+        .then((resp) => resp.json())
+        .then((resp) => {
+            this.setState({Collections : resp});
+        })
+        .catch((error) => {
+            alert("Error could not fetch data.");
+        });
     }
     
     render() {
+
+      if(this.state.Collections != null){
         
+      }
+
       return (
         <main className="flex">
           <Nav />
@@ -38,12 +121,8 @@ class Collections extends React.Component {
                 <div style={{height: "100%"}} className='flex'>
                     <div id="collection-holder">
                         <h4>Collections</h4>
-                        <ul>
-                            <li onClick={this.New}>New</li>
-                        </ul>
-                        <ul id="collection-list">
-                            <li></li>
-                        </ul>
+                        <button onClick={this.New}>New</button>
+                        <ul id="collection-list"></ul>
                     </div>
                     <div id="type-holder">
                         <h4>Types</h4>
