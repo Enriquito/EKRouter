b0284b7994a2afe82d7258920dc533920f044cf1
Enrique Kreuk <Enrique.Kreuk@zzm.local>
2020-03-12 10:34:55 +0100

    Collection creation

--- admin/frontend/src/App.js
+++ admin/frontend/src/App.js
@@ -8,15 +8,19 @@ import Items from './Components/Items';
 import Collection from './Components/Collection';
 import Item from './Components/Item';
 import ItemNew from './Components/ItemNew';
+import CollectionNew from './Components/CollectionNew';
 
 
 function App() {
   return(
       <Router>
         <Route exact path="/" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />  
+        <Route exact path="/dashboard" component={Dashboard} />
+
         <Route exact path="/collections" component={Collections} />
         <Route exact path="/collection/edit/:name" component={Collection} />
+        <Route exact path="/collection/new" component={CollectionNew} />
+
         <Route exact path="/items/:collection" component={Items} />
         <Route exact path="/item/edit/:id" component={Item} />
         <Route exact path="/item/new/collection/:collection" component={ItemNew} />
---
+++ admin/frontend/src/Components/CollectionNew/CollectionNew.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { hasSession, ReplaceSpaces } from '../Helpers';
+import Navigation from '../Navigation';
+
+class CollectionNew extends React.Component {
+    constructor(props){
+        hasSession(false);
+        super(props);
+
+        this.state = {
+          Collection : null
+        };
+    }
+
+    componentDidMount(){
+
+    }
+
+    Save(){
+      const name = document.getElementById("collection-name").value;
+      const description = document.getElementById("collection-description").value;
+
+      fetch('http://localhost/api/collection', {
+          method : "POST",
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ 
+              collection : {
+                name: name,
+                description: description,
+                owner : 14
+              }
+            }
+          )
+      })
+      .then(function(data){
+          if(data.status === 200)
+          {
+            window.location = `/collection/edit/${name}`;
+          }
+          else
+            alert("Error while updating page");
+      })
+      .catch(function(error){
+        alert('error');
+      });
+    }
+  
+    render() {  
+
+      return (
+        <main className="flex">
+          <Navigation item="collections" />
+            <div id="holder">
+                <h1 style={{marginTop: "0px"}}>New Collection</h1>
+                <label>Name</label>
+                <br />
+                <input id="collection-name" 
+                 onChange={(e) => {
+                  e.target.value = ReplaceSpaces(e.target.value, '-')
+                }} 
+                style={{width: "500px"}}  type="text" placeholder="Collection name" />
+                <br />
+                <label>Description</label>
+                <br />
+                <textarea id="collection-description" style={{width: "500px", height: "150px", resize: "vertical"}} onChange={() => {}} ></textarea>
+                <div className="flex">
+                    <button 
+                    style={{width: "75px", height : "30px", padding : "0"}} 
+                    className="theme-green-bg new-collection-button"
+                    onClick={this.Save.bind(this)}
+                    >Save</button>
+                </div>
+            </div>
+        </main>
+      );
+    }
+  }
+
+export default CollectionNew;
