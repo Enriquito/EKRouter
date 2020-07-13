eaa1c5d5fa94668ff6909e17a321b233266baff8
Enrique <enrique.kreuk@icloud.com>
2020-07-13 08:13:02 +0200

    Update Settings.php

--- src/Settings.php
+++ src/Settings.php
@@ -3,9 +3,9 @@
 define("DB_USE", true);
 define("DB_DEBUG", true);
 define("DB_HOST", "quit0.nl");
-define("DB_USERNAME", "u42208p37453_fit");
-define("DB_NAME","u42208p37453_fit");
-define("DB_PASSWORD", "Jg1YHO5gY0");
+define("DB_USERNAME", "");
+define("DB_NAME","");
+define("DB_PASSWORD", "");
 define("DB_CHARSET", 'utf8');
 
 define("ROUTE_TABLE_LOCATION", 'src/Q/App/Routes.php');
